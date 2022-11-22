3feed9586096c7977a7c195b137da236a366195d
Ayushi <chancahlneekhra@gmail.com>
2022-11-22 10:03:55 +0530

    login css

--- style.js
+++ style.js
@@ -1,6 +1,7 @@
 const bar=document.getElementById('bar');
 const close=document.getElementById('close');
 const nav=document.getElementById('navbar');
+const loginBtn=document.getElementById('loginBtn');
 
 if(bar){
     bar.addEventListener('click',()=>{
@@ -13,4 +14,7 @@ if(close){
         nav.classList.remove('active');
     }
     );
+}
+const signIn=()=>{
+    
 }
